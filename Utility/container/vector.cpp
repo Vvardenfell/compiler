90a118f42c0882f85210eb2f242222d4b8960775
Vvardenfell <Vvardenfell@users.noreply.github.com>
2016-10-30 03:33:05 +0100

    Removed test code
       * Removed test code for Vector

--- Utility/container/vector.cpp
+++ Utility/container/vector.cpp
@@ -1,34 +1,3 @@
 #include "vector.h"
-#include <stdio.h>
 
 template<typename T> const double Vector<T>::RESIZE_FACTOR = 1.4;
-
-#include <vector>
-#include <iostream>
-
-int main() {
-	//std::vector<char> vec;
-	Vector<char> vec;
-	int i = 1;
-
-	for (int j = 0 ; j < 4000000; j++) {
-		vec.push_back(static_cast<char>(j&0xff));
-		if (j%1000==0)printf("%i\n",j);
-	}
-
-
-	std::cout << vec.size() << std::endl;
-	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
-		printf("%i\n", *iter);
-	}
-
-
-
-
-return 0;
-}
-
-/*
-909
-548
-164*/
