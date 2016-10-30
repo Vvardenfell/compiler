72a7cfa39e6e79ceaa1881d799ed5b503ba72d88
Vvardenfell <Vvardenfell@users.noreply.github.com>
2016-10-30 03:30:48 +0100

    Optimized Vector implementation
       * Applied several optimizations
       * Modified Vector to use uninitialized memory

--- Utility/container/vector.cpp
+++ Utility/container/vector.cpp
@@ -3,18 +3,21 @@
 
 template<typename T> const double Vector<T>::RESIZE_FACTOR = 1.4;
 
+#include <vector>
+#include <iostream>
 
 int main() {
-	
-	Vector<int> vec;
+	//std::vector<char> vec;
+	Vector<char> vec;
 	int i = 1;
 
-	for (int j = 0 ; j < 20; j++) {
-		vec.push_back(j);
+	for (int j = 0 ; j < 4000000; j++) {
+		vec.push_back(static_cast<char>(j&0xff));
+		if (j%1000==0)printf("%i\n",j);
 	}
 
 
-	//std::cout << vec.size() << std::endl;
+	std::cout << vec.size() << std::endl;
 	for (auto iter = vec.begin(); iter != vec.end(); iter++) {
 		printf("%i\n", *iter);
 	}
@@ -24,3 +27,8 @@ int main() {
 
 return 0;
 }
+
+/*
+909
+548
+164*/
--- Utility/container/vector.h
+++ Utility/container/vector.h
@@ -1,16 +1,19 @@
-/*
- * vector
- *
- *  Created on: 21.10.2016
- *      Author: Tim
- */
-
 #ifndef VECTOR_H
 #define VECTOR_H
 
 #include <cstdlib>
 #include <utility>
+#include <iterator>
+#include <algorithm>
+#include <type_traits>
 
+template<typename T> class Vector;
+
+template<typename T> void swap(Vector<T>& left, Vector<T>& right) {
+	std::swap(left.objects, right.objects);
+	std::swap(left.next_free_space, right.next_free_space);
+	std::swap(left.end_free_space, right.end_free_space);
+}
 
 template<typename T>
 class Vector {
@@ -18,40 +21,60 @@ private:
 	T* objects;
 	T* next_free_space;
 	T* end_free_space;
-	const static size_t INITIAL_CAPACITY = 16;
+	const static std::size_t INITIAL_CAPACITY = 16;
 	const static double RESIZE_FACTOR;
 
 	void resize() {
-		resize(static_cast<size_t>(this->end_free_space - this->objects * RESIZE_FACTOR));
+		resize(static_cast<std::size_t>((this->end_free_space - this->objects) * RESIZE_FACTOR));
 	}
 
-	void resize(size_t new_capacity) {
-		Vector tmp(begin(), end(), new_capacity);
+	void resize(std::size_t new_capacity) {
+		Vector tmp(this->begin(), this->end(), new_capacity);
 		swap(*this, tmp);
+	}
+
+	void resize_on_demand(size_t required_space) {
+		if (this->end_free_space - this->next_free_space < required_space) {
+			this->resize((this->size() + required_space) * RESIZE_FACTOR);
+		}
+	}
+
+	template<typename U = T> typename std::enable_if<std::is_pod<U>::value>::type destruct() {} // TODO: use std::is_trivially_copyable<T>::value in g++ 5 instead
+	template<typename U = T> typename std::enable_if<!std::is_pod<U>::value>::type destruct() { // TODO: use std::is_trivially_copyable<T>::value in g++ 5 instead
+		reverse_iterator end = this->rend();
+		for (reverse_iterator iterator = this->rbegin(); iterator < end; ++iterator) {
+			try {
+				iterator->~U();
+			}
+			catch(...) {}
+		}
+
+		::operator delete(this->objects);
 	}
 
 public:
 
-	friend void swap(Vector<T>& left, Vector<T>& right);
+	friend void swap<>(Vector<T>& left, Vector<T>& right);
 
+	typedef T value_type;
 	typedef T* iterator;
+	typedef std::reverse_iterator<iterator> reverse_iterator;
 	typedef const T* const_iterator;
-	typedef ptrdiff_t difference_type;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+	typedef std::ptrdiff_t difference_type;
 
-	explicit Vector(size_t capacity = INITIAL_CAPACITY) {
-		this->next_free_space = this->objects = new T[capacity];
+	explicit Vector(std::size_t capacity = INITIAL_CAPACITY) {
+		this->next_free_space = this->objects = static_cast<T*>(::operator new(sizeof(T) * capacity));
 		this->end_free_space = this->objects + capacity;
 	}
 
-	Vector(iterator begin, iterator end) : Vector(end - begin) {
-		insert(begin(), begin, end);
+	template<typename InputIterator> Vector(InputIterator begin, InputIterator end) : Vector(begin, end, std::distance(begin, end) * RESIZE_FACTOR) {}
+
+	template<typename InputIterator> Vector(InputIterator begin, InputIterator end, std::size_t capacity) : Vector(capacity) {
+		this->next_free_space = std::uninitialized_copy(begin, end, this->begin());
 	}
 
-	Vector(iterator begin, iterator end, size_t capacity) : Vector(capacity) {
-		insert(this->begin(), begin, end);
-	}
-
-	size_t size() const {
+	std::size_t size() const {
 		return this->next_free_space - this->objects;
 	}
 
@@ -63,54 +86,67 @@ public:
 		return this->next_free_space;
 	}
 
-	T& operator[](size_t index) {
+	const_iterator begin() const {
+		return this->objects;
+	}
+
+	const_iterator end() const {
+		return this->next_free_space;
+	}
+
+	reverse_iterator rbegin() {
+		return std::reverse_iterator<iterator>(this->end());
+	}
+
+	reverse_iterator rend() {
+		return std::reverse_iterator<iterator>(this->begin());
+	}
+
+	const_reverse_iterator rbegin() const {
+		return std::reverse_iterator<const_iterator>(this->end());
+	}
+
+	const_reverse_iterator rend() const {
+		return std::reverse_iterator<const_iterator>(this->begin());
+	}
+
+	T& operator[](std::size_t index) {
 		return this->objects[index];
 	}
 
-	const T& operator[](size_t index) const {
+	const T& operator[](std::size_t index) const {
 		return this->objects[index];
 	}
 
-	iterator insert(iterator target, const_iterator begin, const_iterator end) {
-		size_t element_count = end - begin;
-		difference_type offset = target - this->begin();
+	template<typename InputIterator> iterator insert(iterator target, InputIterator begin, InputIterator end) {
+		std::size_t element_count = std::distance(begin, end);
+		difference_type offset = std::distance(this->begin(), target);
 
-		if (free_capacity() < element_count) {
-			resize(size() + element_count);
-		}
+		this->resize_on_demand(element_count);
 		target = this->begin() + offset;
 
-		for (iterator copy = this->end(); --copy >= target; ) {
-			*(copy + element_count) = *copy;
-		}
+		std::copy_backward(target, this->end(), this->end() + element_count);
 		this->next_free_space += element_count;
-
-		for ( ; begin < end; begin++, target++) {
-			*target = *begin;
-		}
+		std::copy(begin, end, target);
 
 		return target;
 	}
 
-	size_t free_capacity() {
-		return end_free_space - next_free_space;
+	std::size_t capacity() const {
+		return this->end_free_space - this->objects;
 	}
 	
 	void push_back(const T& object) {
-		insert(end(), &object, (&object) + 1);
+		resize_on_demand(1);
+		std::uninitialized_copy(&object, (&object) + 1, this->end());
+		++this->next_free_space;
 	}
 
 	~Vector() {
-		delete[] this->objects;
+		this->destruct();
 	}
+
 };
-
-
-template<typename T> void swap(Vector<T>& left, Vector<T>& right) {
-	std::swap(left.objects, right.objects);
-	std::swap(left.next_free_space, right.next_free_space);
-	std::swap(left.end_free_space, right.end_free_space);
-}
 
 
 #endif /* VECTOR_H */
