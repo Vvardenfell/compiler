e772445453d892e6173c2baa9e0f1549d3975f7d
unknown <erma1015@IWI-I2-01.ads.hs-karlsruhe.de>
2016-11-04 15:53:37 +0100

    Added testfile
       * Added Testfile for Buffer

--- Buffer/src/TestBuffer.cpp
+++ Buffer/src/TestBuffer.cpp
@@ -1,10 +1,24 @@
 #include "../includes/Buffer.h"
-
+#include <iostream>
 
 int main(int argc, char **argv) {
 
-	Buffer*  buffer;
+	std::cout << ichhabkeinelustmehr() << std::endl;
 
-	buffer = new Buffer();
+}
 
+bool ichhabkeinelustmehr() {
+	char a[] = {"abcdefghijklmnopqrstuvwxyz0123456789"};
+	Buffer test_buffer_size_5("testfile.txt", 5);
+	int i = 0;
+
+	try{
+		while(true) {
+			if(a[i] != test_buffer_size_5.get()) return false;
+			i++;
+		}
+	} catch(...) {
+
+	return true;
+	}
 }
